d7460678b3647381310d0b11194e791ed705fee0
Shanthosh Pushparajah <spushpar@uwaterloo.com>
2018-04-21 14:34:39 -0400

    Add AppIcon

--- LottoTime/Controller/LottoViewController.swift
+++ LottoTime/Controller/LottoViewController.swift
@@ -8,17 +8,59 @@
 
 import Foundation
 import UIKit
+import UserNotifications
 
 class LottoViewController: UIViewController {
     
+    let dailyNotification : Int = 86400
     
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
     //TODO: Overide
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
+                
+                if error != nil {
+                    print("Authorization Unsuccessfull")
+                } else {
+                    print("Authorization successfull")
+                }
+            }
+        }
     
+    
+    @IBAction func btnPressed(_ sender: UIButton) {
+    
+        timedNotifications(inSeconds: TimeInterval(dailyNotification)) { (success) in
+            if success {
+                print("Success")
+            }
+        }
+  }
+
+func timedNotifications (inSeconds : TimeInterval, completion: @escaping (_ Sender: Bool) -> ()) {
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: true)
+
+        let content = UNMutableNotificationContent()
+
+        content.title = "Daily Check"
+        content.subtitle = "Check Nation Wide Lottery"
+        content.body = "Check out 'Lotto Time', it may change your life"
+
+        let request = UNNotificationRequest(identifier: "customNotification", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if (error != nil) {
+                completion(false)
+            } else {
+                completion(true)
+            }
+        }
+        
+    }
+
 }
 
